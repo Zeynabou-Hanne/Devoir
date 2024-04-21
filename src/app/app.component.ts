2fcdd9cd1c8b6b39fa977b96b928b59d635684cd
Zeynabou <hanne.zeynabou@ugb.edu.sn>
2024-04-21 17:39:14 +0000

    le devoir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+interface Class {
+  name: string;
+}
 
+interface Student {
+  name: string;
+  className: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eadcyDemo';
+  classes: Class[] = [
+    { name: 'Terminal S1' },
+    { name: 'Terminal S2' },
+    { name: 'Terminal L1a' },
+    { name: 'Terminal L2b' }
+  ];
+
+  students: Student[] = [
+    { name: 'Penda Sarr', className: 'Terminal S2' }
+  ];
+
+  newClassName: string = '';
+  newStudentName: string = '';
+  selectedClass: string = '';
+
+  addClass() {
+    this.classes.push({ name: this.newClassName });
+    this.newClassName = '';
+  }
+  addStudent() {
+    this.students.push({ name: this.newStudentName, className: this.selectedClass });
+    this.newStudentName = '';
+    this.selectedClass = '';
+  }
+
+  deleteClass(index: number) {
+    this.classes.splice(index, 1);
+  }
+  deleteStudent(index: number) {
+    this.students.splice(index, 1);
+  }
 }
